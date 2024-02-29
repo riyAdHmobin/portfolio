3e5b9afb8e4e3e6e8a6344701839357817b40cfc
riyadhmobin <riyadhmobin@gmail.com>
2024-02-29 12:57:21 +0600

    concept

---
+++ assets/js/data.js
@@ -0,0 +1,7 @@
+var hero_section_name = "Riyadh Mobin";
+
+    // Select the HTML elements where you want to insert the variables
+    var nameContainer = document.getElementById("hero_section");
+
+    // Modify the content of the selected elements
+    nameContainer.innerHTML = "<a>" + hero_section_name + "</a>";
